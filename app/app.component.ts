33fc31cb0c425d73a96a938951bb7c55e437e396
juan jardim <juanjardim@gmail.com>
2017-01-08 16:02:25 +0100

    Create product list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component}  from '@angular/core';
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-        <div>My First Component </div>
+        <pm-products></pm-products>
     </div>
     `
 })
