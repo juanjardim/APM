4a388fc6b0e2e4e7d9cc2bca23650a74a2917f9f
juan jardim <juanjardim@gmail.com>
2017-01-19 19:40:34 +0100

    Create IProduct interface

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
-import {ProductListComponent} from './products/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ 
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
     AppComponent,
-    ProductListComponent ],
-  bootstrap: [ AppComponent ]
+    ProductListComponent]
+  ,
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
